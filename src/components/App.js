2f2e85af521832122ba771c0e04138fc40f9ee5b
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-07-02 14:51:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,20 @@
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+const App = ()=> {
+    let [showprop , setShowprop] = useState(false);
+    return (
+        <div>
+            <div className="model-overlay">
+                <button onClick={()=>setShowprop(true)}>Show Modal</button>
+            { showprop && (<div className="model-close">
+                <button onClick={()=>setShowprop(false)}>Close</button>
+                <p>This is the content of modal.</p>
+            </div>)
+            }   
+            </div>
+        </div>
+    )
 }
 
 export default App
