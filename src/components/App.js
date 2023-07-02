f6c64b8567aeb62528702d0435b759df0d45398b
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-07-02 16:11:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,23 @@ import React ,{useState} from "react";
 import './../styles/App.css';
 
 const App = ()=> {
-    let [showprop , setShowprop] = useState(false);
+    let [show , setShow] = useState(false);
     return (
         <div>
-            <button onClick={()=>setShowprop(true)}>Show Modal</button>
+            <button onClick={()=>setShow(true)}>Show Modal</button>
             {
-            showprop && (<div className="model-overlay" onClick={()=>setShowprop(false)}>
-                <button className="model-close" onClick={()=>setShowprop(false)}>Close</button>
-                <p>This is the content of the modal.</p>
-            </div>)
+            show && (<Modal showprop={setShow} />)
             }
+        </div>
+    )
+}
+
+function Modal({showprop}) {
+    
+    return (
+        <div className="model-overlay" onClick={()=>showprop(false)}>
+            <button className="model-close" onClick={()=>showprop(false)}>Close</button>
+            <p>This is the content of the modal.</p>
         </div>
     )
 }
